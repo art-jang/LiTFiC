85206944dbc8b307f966fefcda3b18e0eecf43ee
Haran Raajesh <haran@gnodem1.triton.cluster>
2024-08-06 18:40:38 +0100

    Triton setup

---
+++ scripts/triton.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=llama                   # Job name
+#SBATCH --mail-type=END,FAIL                # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=haran@robots.ox.ac.uk    # Where to send mail
+#SBATCH --nodes=1                          # Node count
+#SBATCH --ntasks-per-node=2                         # Total number of tasks across all nodes
+#SBATCH --cpus-per-task=10                   # Number of CPU cores per task
+#SBATCH --mem-per-cpu=24G                         # Job memory request
+#SBATCH --time=20:00:00                     # Time limit hrs:min:sec
+#SBATCH --partition=ddp-2way                   # Partition (compute (default) / gpu)
+#SBATCH --gres=gpu:2                       # Requesting 4 GPUs
+#SBATCH --constraint=a6000      # Request nodes with specific features
+
+
+. ~/miniconda3/etc/profile.d/conda.sh
+conda activate slt
+
+export HYDRA_FULL_ERROR=1 
+export CUDA_LAUNCH_BLOCKING=1
+
+srun python src/train.py task_name=llama_sub1_lora experiment=llama3_haran paths=haran_triton
